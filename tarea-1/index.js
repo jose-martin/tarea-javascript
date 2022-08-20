c3eedf1374225666cf2ce52d3ea6bc9ff93a49b1
Gabriel Caycho <gabrielcaychojose8@hotmail.com>
2022-08-20 00:34:00 -0500

    practicando js

---
+++ tarea-1/index.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 3; i++) {
+    let usuario = prompt("ingrese su usuario");
+    let contraseña = prompt("ingrese su contraseña");
+    if ((usuario == "martin") && (contraseña == 123)) {
+        alert(`Bienvenido ${usuario}`);
+        break;
+    } else {
+        alert("Usuario Incorrecto");
+        alert(`intento ${i}`);
+    }
+    if (i == 3) {
+        let respuesta = prompt("Deseas recuperar tus datos S (si) N (no)");
+
+        if (respuesta == "S") {
+            alert("usuario martin - contraseña 123");
+        }
+
+    }
+}
